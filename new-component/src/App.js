f1d24c4ea1309d2ff0b9150dd43f45a2f1680c8e
Tazeem041 <124208833+Tazeem041@users.noreply.github.com>
2024-03-27 18:48:20 +0000

    CreatNewComponenet

---
+++ new-component/src/App.js
@@ -0,0 +1,17 @@
+function Profile() {
+  return (
+    <img
+      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR77iOanUEwD6cR1bth7E0y0jnAJCnDH6Zp1Q&usqp=CAU"alt="" /> 
+      );
+}
+
+export default function picture () {
+  return (
+    <div>
+      <h1>profile pic</h1>
+      <Profile />
+      <Profile />
+    
+    </div>
+  );
+}
